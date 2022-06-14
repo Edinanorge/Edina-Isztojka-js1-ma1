e21c8157fdf259d3d74233aa8a9daa6457ceb2ec
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2022-06-14 20:58:53 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const cats = [
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
+];
+
+const cat = {
+  complain: function () {
+    console.log("Meow!");
+  },
+};
