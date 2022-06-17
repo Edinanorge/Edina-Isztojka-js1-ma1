f7120343a9d395f67475cbd2d049315f2ec1eb24
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2022-06-17 11:33:58 +0200

    readme.md created

--- js/script.js
+++ js/script.js
@@ -11,9 +11,32 @@ const cats = [
     age: 21,
   },
 ];
-
+// Question 1
 const cat = {
-  complain: function () {
-    console.log("Meow!");
-  },
+  complain: isTheCatComplaining(),
 };
+
+function isTheCatComplaining() {
+  console.log("Meow!");
+}
+
+// Question 2
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+heading.classList.add("subheading");
+// Question 5
+const paragraphs = document.querySelectorAll("p");
+for (i = 0; i < paragraphs.length; i++) {
+  paragraphs[i].style.color = "red";
+}
+// Question 6
+// Question 7
+// Question 8
